43f1fb3a9c728b4305e73659ed5e2a7d3876175a
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-02-03 20:36:36 +0900

    空のファイルを新規作成

---
+++ Include/Hashes/MD5/MD5.h
@@ -0,0 +1,25 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---   Hash Functions Library   ---                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of MD5 class.
+**
+**      @file       MD5/MD5.h
+**/
+
+#if !defined( HASHES_MD5_INCLUDED_MD5_H )
+#    define   HASHES_MD5_INCLUDED_MD5_H
+
+
+#endif
