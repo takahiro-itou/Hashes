3cb5484e17537a66d99412c2cac8a368054de3fe
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-02-05 22:49:50 +0900

    空のヘッダファイルを新規作成

---
+++ Include/Hashes/Common/HashesTypes.h
@@ -0,0 +1,31 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---   Hash Functions Library   ---                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      Type Definitions.
+**
+**      @file       Common/HashesTypes.h
+**/
+
+#if !defined( HASHES_COMMON_INCLUDED_HASHES_TYPES_H )
+#    define   HASHES_COMMON_INCLUDED_HASHES_TYPES_H
+
+#include    "HashesSettings.h"
+
+
+HASHES_NAMESPACE_BEGIN
+
+HASHES_NAMESPACE_END
+
+#endif
