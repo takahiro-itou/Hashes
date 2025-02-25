285a32e77be11bfd3c8ee7d05ab2166a105a89c4
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-02-25 20:01:59 +0900

    空のソースファイルを新規作成

---
+++ Bin/MD5Driver.cpp
@@ -0,0 +1,30 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---   Hash Functions Library   ---                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      サンプルプログラム。
+**
+**      @file       Bin/MD5Driver.cpp
+**/
+
+#include    "Hashes/MD5/MD5.h"
+
+#include    <iostream>
+
+using   namespace   HASHES_NAMESPACE;
+
+int  main(int argc, char * argv[])
+{
+    return ( 0 );
+}
