a5adccd932c08d440571bc328f7a554c783b19c9
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-02-27 20:19:06 +0900

    コマンドラインオプションの解析を仮実装

--- Bin/MD5Driver.cpp
+++ Bin/MD5Driver.cpp
@@ -20,11 +20,57 @@
 
 #include    "Hashes/MD5/MD5.h"
 
+#include    <getopt.h>
 #include    <iostream>
+
 
 using   namespace   HASHES_NAMESPACE;
 
 int  main(int argc, char * argv[])
 {
+    static struct option long_opts[] = {
+        { "binary", no_argument, 0, 'b' },
+        { "check",  no_argument, 0, 'c' },
+        { "text" ,  no_argument, 0, 't' },
+        { "ignore-missing", no_argument, 0, 0 },
+        { 0, 0, 0, 0 },
+    };
+    int     c;
+    int     lo_index;
+
+    while ( (c = getopt_long(argc, argv, "bct", long_opts, &lo_index)) != -1 )
+    {
+        switch ( c ) {
+        case  0:
+            switch ( lo_index ) {
+            case  3:
+                std::cout   <<  "option: ignore-missing"    <<  std::endl;
+                break;
+            default:
+                std::cerr   <<  "unknown option."   <<  std::endl;
+                break;
+            }
+            break;
+        case  'b':
+            std::cout   <<  "option: -b (--binary)" <<  std::endl;
+            break;
+        case  'c':
+            std::cout   <<  "option: -c (--check)"  <<  std::endl;
+            break;
+        case  't':
+            std::cout   <<  "option: -t (--text)"   <<  std::endl;
+            break;
+        default:
+            std::cerr   <<  "Invalid Option:"
+                        <<  std::endl;
+            break;
+        }
+    }
+
+    for ( ; optind < argc; ++ optind ) {
+        std::cout   <<  "non-option :"
+                    <<  argv[optind]    <<  std::endl;
+    }
+
     return ( 0 );
 }
