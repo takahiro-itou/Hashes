98536c2ffffb3c5da0003d4470f788efe4a75a87
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-03-03 20:26:19 +0900

    空のヘッダファイルを新規作成

---
+++ Include/Hashes/Common/AppOpts.h
@@ -0,0 +1,48 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---   Hash Functions Library   ---                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of AppOpts struct.
+**
+**      @file       Common/AppOpts.h
+**/
+
+#if !defined( HASHES_COMMON_INCLUDED_APP_OPTS_H )
+#    define   HASHES_COMMON_INCLUDED_APP_OPTS_H
+
+#if !defined( HASHES_COMMON_INCLUDED_HASHES_TYPES_H )
+#    include    "HashesSettings.h"
+#endif
+
+#if !defined( HASHES_SYS_INCLUDED_STRING )
+#    include    <string>
+#    define   HASHES_SYS_INCLUDED_STRING
+#endif
+
+#if !defined( HASHES_SYS_INCLUDED_VECTOR )
+#    include    <vector>
+#    define   HASHES_SYS_INCLUDED_VECTOR
+#endif
+
+
+HASHES_NAMESPACE_BEGIN
+
+//========================================================================
+//
+//    Type Definitions.
+//
+
+HASHES_NAMESPACE_END
+
+#endif
