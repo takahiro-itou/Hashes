985f91aa778838fcf4661565b3e73b3e3c42c787
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-03-05 22:24:59 +0900

    空のソースファイルを新規作成

---
+++ Lib/Common/AppOpts.cpp
@@ -0,0 +1,33 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---   Hash Functions Library   ---                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of AppOpts class.
+**
+**      @file       Common/AppOpts.cpp
+**/
+
+#include    "Hashes/Common/AppOpts.h"
+
+
+HASHES_NAMESPACE_BEGIN
+namespace  Common  {
+
+namespace  {
+
+}   //  End of (Unnamed) namespace.
+
+
+}   //  End of namespace  Common
+HASHES_NAMESPACE_END
