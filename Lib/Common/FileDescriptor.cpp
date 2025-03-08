4be083a57a14fcb1fbbcc66815295ec9db66afe8
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-03-08 16:52:45 +0900

    メンバ関数を実装

--- Lib/Common/FileDescriptor.cpp
+++ Lib/Common/FileDescriptor.cpp
@@ -20,6 +20,8 @@
 
 #include    "Hashes/Common/FileDescriptor.h"
 
+#include    <sys/stat.h>
+
 
 HASHES_NAMESPACE_BEGIN
 namespace  Common  {
@@ -78,6 +80,71 @@ FileDescriptor::~FileDescriptor()
 //
 //    Public Member Functions (Virtual Functions).
 //
+
+//----------------------------------------------------------------
+//    ファイルを閉じる。
+//
+
+ErrCode
+FileDescriptor::closeFile()
+{
+    if ( this->m_descriptor < 0 ) {
+        //  ファイルが開かれていないので、何もすることが無い。  //
+        return ( ErrCode::SUCCESS );
+    }
+
+    close(this->m_descriptor);
+    this->m_descriptor  = -1;
+
+    return ( ErrCode::SUCCESS );
+}
+
+//----------------------------------------------------------------
+//    ファイルを開く。
+//
+
+ErrCode
+FileDescriptor::openFile(
+        const  std::string  &fileName,
+        const  int          mode)
+{
+    struct stat stbuf;
+
+    //  現在開かれているファイルを閉じる。  //
+    closeFile();
+
+    //  指定されたファイルを開く。  //
+    int fd  = open(fileName.c_str(), mode);
+    if ( fd == -1 ) {
+        return ( ErrCode::FILE_OPEN_ERROR );
+    }
+
+    //  ファイルサイズを取得しておく。  //
+    if ( fstat(fd, &stbuf) == -1 ) {
+    }
+    this->m_cbFileSize  = stbuf.st_size;
+
+    //  ファイルディスクリプタを記憶。  //
+    this->m_descriptor  = fd;
+    this->m_fileName    = fileName;
+    this->m_oModeFlags  = mode;
+
+    return ( ErrCode::SUCCESS );
+}
+
+//----------------------------------------------------------------
+//    最後に使ったファイルを開きなおす。
+//
+
+ErrCode
+FileDescriptor::reopenFile()
+{
+    if ( this->m_fileName.empty() ) {
+        return ( ErrCode::FILE_OPEN_ERROR );
+    }
+
+    return  openFile(this->m_fileName, this->m_oModeFlags);
+}
 
 //========================================================================
 //
