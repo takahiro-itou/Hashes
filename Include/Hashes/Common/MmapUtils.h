1b24c53744d134fe2def344a1a0505e3f9462385
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-03-08 18:31:00 +0900

    クラスを新規作成

--- Include/Hashes/Common/MmapUtils.h
+++ Include/Hashes/Common/MmapUtils.h
@@ -13,26 +13,16 @@
 *************************************************************************/
 
 /**
-**      An Interface of FileDescriptor class.
+**      An Interface of MmapUtils class.
 **
-**      @file       Common/FileDescriptor.h
+**      @file       Common/MmapUtils.h
 **/
 
-#if !defined( HASHES_COMMON_INCLUDED_FILE_DESCRIPTOR_H )
-#    define   HASHES_COMMON_INCLUDED_FILE_DESCRIPTOR_H
+#if !defined( HASHES_COMMON_INCLUDED_MMAP_UTILS_H )
+#    define   HASHES_COMMON_INCLUDED_MMAP_UTILS_H
 
 #if !defined( HASHES_COMMON_INCLUDED_HASHES_TYPES_H )
 #    include    "HashesTypes.h"
-#endif
-
-#if !defined( HASHED_SYS_INCLUDED_FCNTL_H )
-#    include    <fcntl.h>
-#    define   HASHED_SYS_INCLUDED_FCNTL_H
-#endif
-
-#if !defined( HASHES_SYS_INCLUDED_STRING )
-#    include    <string>
-#    define   HASHES_SYS_INCLUDED_STRING
 #endif
 
 
@@ -41,10 +31,10 @@ namespace  Common  {
 
 //========================================================================
 //
-//    FileDescriptor  class.
+//    MmapUtils  class.
 //
 
-class  FileDescriptor
+class  MmapUtils
 {
 
 //========================================================================
@@ -64,14 +54,14 @@ public:
     **  （デフォルトコンストラクタ）。
     **
     **/
-    FileDescriptor();
+    MmapUtils();
 
     //----------------------------------------------------------------
     /**   インスタンスを破棄する
     **  （デストラクタ）。
     **
     **/
-    virtual  ~FileDescriptor();
+    virtual  ~MmapUtils();
 
 //========================================================================
 //
@@ -92,32 +82,6 @@ public:
 //
 //    Public Member Functions (Virtual Functions).
 //
-public:
-
-    //----------------------------------------------------------------
-    /**   ファイルを閉じる。
-    **
-    **/
-    virtual  ErrCode
-    closeFile();
-
-    //----------------------------------------------------------------
-    /**   ファイルを開く。
-    **
-    **  @param [in] fileName    ファイル名。
-    **  @param [in] mode        オープンモード。
-    **/
-    virtual  ErrCode
-    openFile(
-            const  std::string  &fileName,
-            const  int  mode = O_RDONLY);
-
-    //----------------------------------------------------------------
-    /**   最後に使ったファイルを開きなおす。
-    **
-    **/
-    virtual  ErrCode
-    reopenFile();
 
 //========================================================================
 //
@@ -128,27 +92,6 @@ public:
 //
 //    Accessors.
 //
-public:
-
-    //----------------------------------------------------------------
-    /**   ファイルディスクリプタを取得する。
-    **
-    **/
-    const   int
-    getFileDescriptor()  const
-    {
-        return ( this->m_descriptor );
-    }
-
-    //----------------------------------------------------------------
-    /**   ファイルサイズを取得する。
-    **
-    **/
-    const   FileLength
-    getFileSize()  const
-    {
-        return ( this->m_cbFileSize );
-    }
 
 //========================================================================
 //
@@ -164,19 +107,6 @@ public:
 //
 //    Member Variables.
 //
-private:
-
-    /**   ファイルディスクリプタ。  **/
-    int             m_descriptor;
-
-    /**   ファイル名。  **/
-    std::string     m_fileName;
-
-    /**   ファイルオープンモード。  **/
-    int             m_oModeFlags;
-
-    /**   ファイルサイズ。  **/
-    FileLength      m_cbFileSize;
 
 //========================================================================
 //
@@ -184,7 +114,7 @@ private:
 //
 public:
     //  テストクラス。  //
-    friend  class   FileDescriptorTest;
+    friend  class   MmapUtilsTest;
 };
 
 }   //  End of namespace  Common
---
+++ Lib/Common/MmapUtils.cpp
@@ -0,0 +1,103 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---   Hash Functions Library   ---                  **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of MmapUtils class.
+**
+**      @file       Common/MmapUtils.cpp
+**/
+
+#include    "Hashes/Common/MmapUtils.h"
+
+#include    <sys/stat.h>
+#include    <unistd.h>
+
+
+HASHES_NAMESPACE_BEGIN
+namespace  Common  {
+
+namespace  {
+
+}   //  End of (Unnamed) namespace.
+
+
+//========================================================================
+//
+//    MmapUtils  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （デフォルトコンストラクタ）。
+
+MmapUtils::MmapUtils()
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+MmapUtils::~MmapUtils()
+{
+}
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Accessors.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  Common
+HASHES_NAMESPACE_END
